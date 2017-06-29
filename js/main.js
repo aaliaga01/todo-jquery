62aaa5c809c0bf135ef62feefe2a9f2c1638edfc
aaliaga-casa <a.aliaga.val@gmail.com>
2017-06-29 01:19:34 -0300

    agregar tarea y remover tarea ok

--- js/main.js
+++ js/main.js
@@ -4,17 +4,31 @@ $(document).ready( function (){
 
 		var tarea = document.getElementById("tarea-nueva").value; // tomar el valor de input
 
-		if(tarea.length == 0){
+		/*if(tarea.length == 0){
     		alert("ingresa una tarea");
-			}
+			}*/
 
+		    var parrafo= $("<p class='listado'></p>");
+        	var input= $("<input type='checkbox' id='to-do'/>");
+        	var etiqueta= $("<label for='to-do'></label>");
+        	var boton= $("<button>Remove</button>");
+
+        //condicion que agrega elementos a la lista presionando enter
     	if(e.which == 13) {
         event.preventDefault();
-        	$("#tareas").prepend("<p>Texto añadido al comienzo del párrafo.</p>");
+
+        	$("#tareas").prepend(parrafo);
+        	$(parrafo).prepend(input,etiqueta,boton);
+        	$(etiqueta).text(tarea);
+
     	}
 
-    	
+    	//elimina el elemento seleccionado con el evento click
+    	$("button").click(function(event){
+			$(this).parent().remove();
+		});
 
 	});// .../. keypress
 
-});// .../. ready
+
+	});// .../. ready
